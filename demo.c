b5d42cae63dda67c5527a5e060eee7d46e98d624
shyhshiun <sobig777@gmail.com>
2024-06-15 23:14:37 +0800

    Add files via upload

--- demo.c
+++ demo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "utils/fn.h"
 #include "utils/toml/toml.h"
 
@@ -8,7 +9,7 @@
 
 int main()
 {
-    char*      path = "story.toml";
+    char*      path = "sample.toml";
 
     Item*      items = NULL;
     int        n_item;
@@ -36,59 +37,64 @@ int main()
               &dialogues, &n_dialogue,
               path);
 
-    printf("-------------------------------------------------------\n");
-    printf("\033[35mnumber of item: %d\033[0m\n", n_item);
-    printf("\033[35mnumber of scene: %d\033[0m\n", n_scene);
-    printf("\033[35mnumber of character: %d\033[0m\n", n_character);
-    printf("\033[35mnumber of event: %d\033[0m\n", n_event);
-    printf("\033[35mnumber of dialogue: %d\033[0m\n", n_dialogue);
+    // 開始劇情
+    Event*    event = &(events[0]);
+    Dialogue* dialog = NULL;
+    Act_Type  type = EVENT;
+    int       opt_number;
+    Option    opt;
+    bool      is_ending = false;
 
-    // find the target item
-    Item* item = find_item(items, n_item, "envelope");
-    printf("-------------------------------------------------------\n");
-    printf("Found the item: 'envelope'\n");
-    printf("name: %s\n", item->name);
-    printf("description: %s\n", item->desc);
-    printf("icon: %s\n", item->icon);
+    while (!is_ending) {
+        switch (type) {
+            case EVENT:
+                printf("\033[33m用 SDL 顯示圖:\033[0m %s\n", event->scene);
+                dialog = find_dialogue(dialogues, n_dialogue, event->dialog);
+                type = DIALOGUE;
+                break;
+                
+            case DIALOGUE:
+                printf("\033[35m用 SDL 顯示對話:\033[0m %s\n", dialog->text);
+                if (dialog->character) {
+                    printf("用 SDL 顯示角色: %s\n", dialog->character);
+                }
 
-    // find the target scene
-    Scene* scene = find_scene(scenes, n_scene, "street");
-    printf("-------------------------------------------------------\n");
-    printf("Found the scene: 'street'\n");
-    printf("name: %s\n", scene->name);
-    printf("background: %s\n", scene->bg);
+                if (dialog->n_opt > 0) {
+                    printf("\033[32m------------------------ 出現選項 ------------------------\033[0m\n");
+                    for (int i = 0; i < dialog->n_opt; i++) {
+                        printf("選項(%d): %s\n", i + 1, dialog->opts[i].text);
+                    }
 
-    // find the target character
-    Character* character = find_character(characters, n_character, "frienddad");
-    printf("-------------------------------------------------------\n");
-    printf("Found the character: 'frienddad'\n");
-    printf("name: %s\n", character->name);
-    printf("avatar: %s\n", character->avatar);
-    printf("tachie: %s\n", character->tachie);
+                    printf("請選擇一個, 你選擇: ");
+                    scanf("%d", &opt_number);
+                    opt_number = opt_number - 1 >= dialog->n_opt ? dialog->n_opt - 1 : opt_number - 1;
+                    opt = dialog->opts[opt_number];
 
-    // find the target event
-    Event* event = find_event(events, n_event, "findtreasure");
-    printf("-------------------------------------------------------\n");
-    printf("Found the event: 'findtreasure'\n");
-    printf("scene: %s\n", event->scene);
-    printf("dialog: %s\n", event->dialog);
+                    if (opt.event) {
+                        event = find_event(events, n_event, opt.event);
+                        type = EVENT;
+                    }
+                    else if (opt.next) {
+                        dialog = find_dialogue(dialogues, n_dialogue, opt.next);
+                        type = DIALOGUE;
+                    }
+                    else {
+                        printf("\033[31mdialogue's option format is error: '%s'\033[0m\n", dialog->alias);
+                    }
+                }
 
-    // find the target dialogue
-    Dialogue* dialogue = find_dialogue(dialogues, n_dialogue, "scene23");
-    printf("-------------------------------------------------------\n");
-    printf("Found the dialogue: 'scene23'\n");
-    if (dialogue->character) {
-        printf("character: %s\n", dialogue->character);
-    }
-    printf("text: %s\n", dialogue->text);
-    for (int i = 0; i < dialogue->n_opt; i++) {
-        printf("option (%d):\n", i + 1);
-        printf("    text: %s\n", dialogue->opts[i].text);
-        if (dialogue->opts[i].event) {
-            printf("    event: %s\n", dialogue->opts[i].event);
+                printf("\n");
+
+                if (dialog && strstr(dialog->alias, "ending")) {
+                    is_ending = true;
+                }
+
+                break;
+
+            default:
+                printf("\033[31mUnknow type\033[0m\n");
         }
-        if (dialogue->opts[i].next) {
-            printf("    next: %s\n", dialogue->opts[i].next);
-        }
     }
+
+    printf("\033[35m劇終囉 !!!\033[0m\n");
 }

d45c8e0d83aa6e35dc7cf07c8632bbe2f9ab024a
shyhshiun <sobig777@gmail.com>
2024-06-16 13:50:00 +0800

    Add files via upload

--- demo_can_show_image.c
+++ demo_can_show_image.c
@@ -2,214 +2,259 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
 #include "utils/fn.h"
 #include "utils/toml/toml.h"
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <math.h>
 
-// 初始化SDL和全局变量
-int initSDL();
-void closeSDL();
-SDL_Texture* loadTexture(const char* path);
-void displayScene(SDL_Renderer* renderer, SDL_Texture* texture);
-void displayText(SDL_Renderer* renderer, const char* text, int x, int y, SDL_Color color);
-void displayDialogueBox(SDL_Renderer* renderer, int x, int y, int w, int h);
-void displayCharacter(SDL_Renderer* renderer, SDL_Texture* texture);
+#define SCREEN_WIDTH 480
+#define SCREEN_HEIGHT 640
+
+int init();
+int loadMedia(char* imagePath1);
+void close();
+
+SDL_Surface* loadSurface(const char* path);
 SDL_Window* gWindow = NULL;
-SDL_Renderer* gRenderer = NULL;
-TTF_Font* gFont = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gHelloWorld = NULL;
+SDL_Surface* gOverlayImage = NULL;
 
-int main() {
-    char* path = "story.toml";
+int main()
+{
+    char*      path = "story.toml";
 
-    // 解析TOML配置文件
-    Item* items = NULL;
-    int n_item;
-    Scene* scenes = NULL;
-    int n_scene;
+    Item*      items = NULL;
+    int        n_item;
+
+    Scene*     scenes = NULL;
+    int        n_scene;
+
     Character* characters = NULL;
-    int n_character;
-    Event* events = NULL;
-    int n_event;
-    Dialogue* dialogues = NULL;
-    int n_dialogue;
+    int        n_character;
+
+    Event*     events = NULL;
+    int        n_event;
+
+    Dialogue*  dialogues = NULL;
+    int        n_dialogue;
+
     Player player;
-    read_toml(&player, &items, &n_item, &scenes, &n_scene, &characters, &n_character, &events, &n_event, &dialogues, &n_dialogue, path);
 
-    if (initSDL() == -1) return -1;  // 初始化SDL
+    // read all the information in toml
+    read_toml(&player, 
+              &items, &n_item,
+              &scenes, &n_scene,
+              &characters, &n_character,
+              &events, &n_event,
+              &dialogues, &n_dialogue,
+              path);
 
-    bool quit = false;
-    SDL_Event e;
-    Event* event = &events[0];
+    // 開始劇情
+    Event*    event = &(events[0]);
     Dialogue* dialog = NULL;
-    Act_Type type = EVENT;
-    int opt_number;
-    Option opt;
-    SDL_Texture* currentTexture = NULL;
-    SDL_Texture* characterTexture = NULL; // 人物纹理
-    bool end_scene = false;  // 用于控制结束时暂停
+    Act_Type  type = EVENT;
+    int       opt_number;
+    Option    opt;
+    bool      is_ending = false;
+    char*     img_path = NULL;
 
-    while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-            }
-        }
-
-        if (type == EVENT) {
-            currentTexture = loadTexture(find_scene(scenes, n_scene, event->scene)->bg);
-            SDL_RenderClear(gRenderer);
-            displayScene(gRenderer, currentTexture);
-            dialog = find_dialogue(dialogues, n_dialogue, event->dialog);
-            type = DIALOGUE;
-        } else if (type == DIALOGUE) {
-            SDL_RenderClear(gRenderer);
-            displayScene(gRenderer, currentTexture); // 重新绘制背景图片
-            
-            // 如果对话中有角色，则加载并显示角色图像
-            if (dialog->character) {
-                characterTexture = loadTexture(find_character(characters, n_character, dialog->character)->avatar);
-                displayCharacter(gRenderer, characterTexture);
-            }
-
-            displayDialogueBox(gRenderer, 0, SCREEN_HEIGHT - 150, SCREEN_WIDTH, 150);
-            displayText(gRenderer, dialog->text, 50, SCREEN_HEIGHT - 130, (SDL_Color){255, 255, 255, 255});
-            SDL_RenderPresent(gRenderer);
-
-            if (dialog->n_opt > 0) {
-                for (int i = 0; i < dialog->n_opt; i++) {
-                    printf("Option (%d): %s\n", i + 1, dialog->opts[i].text);
+    while (!is_ending) {
+        switch (type) {
+            case EVENT:
+                img_path = find_scene(scenes, n_scene, event->scene)->bg;
+                printf("\033[33m用 SDL 顯示圖:\033[0m %s\n", img_path);
+                
+                if (!loadMedia(img_path)) {
+                    printf("Failed to load media!\n");
+                } else {
+                    printf("Media loaded successfully.\n");
                 }
-                printf("Choose an option: ");
-                scanf("%d", &opt_number);
-                opt_number = (opt_number - 1 >= dialog->n_opt) ? dialog->n_opt - 1 : opt_number - 1;
-                opt = dialog->opts[opt_number];
-                if (opt.event) {
-                    event = find_event(events, n_event, opt.event);
-                    type = EVENT;
-                } else if (opt.next) {
-                    dialog = find_dialogue(dialogues, n_dialogue, opt.next);
-                    type = DIALOGUE;
-                }
-                SDL_RenderClear(gRenderer);
-            }
 
-            if (dialog && strstr(dialog->alias, "ending")) {
-                end_scene = true;
+                if( !init() )
+                {
+                    printf( "初始化失敗！\n" );
+                }
+                else
+                {
+                    // 加載媒體
+                    if( !loadMedia(img_path) )
+                    {
+                        printf( "加載媒體失敗！\n" );
+                    }
+                    else
+                    {
+                        // 應用圖像
+                        SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL );
+
+                        //Apply the image stretched
+                        SDL_Rect stretchRect;
+                        stretchRect.x = 0;
+                        stretchRect.y = 0;
+                        stretchRect.w = SCREEN_WIDTH;
+                        stretchRect.h = SCREEN_HEIGHT;
+                        SDL_BlitScaled( gHelloWorld, NULL, gScreenSurface, &stretchRect );
+
+                        // 疊加第二張圖像
+                        SDL_Rect overlayRect;
+                        overlayRect.x = 280; // 設定第二張圖像的 x 坐標
+                        overlayRect.y = 0; // 設定第二張圖像的 y 坐標
+                        overlayRect.w = 200; // 設定第二張圖像的寬度
+                        overlayRect.h = 200; // 設定第二張圖像的高度
+                        SDL_BlitScaled(gOverlayImage, NULL, gScreenSurface, &overlayRect);
+
+                        //Update the surface
+                        SDL_UpdateWindowSurface( gWindow );
+
+                        SDL_Event e; 
+                        int quit = 0; 
+                        while( quit == 0 )
+                        { 
+                            while( SDL_PollEvent( &e ) )
+                            { 
+                                if( e.type == SDL_QUIT ) quit = 1; 
+                            } 
+                        }
+                    }
+                }
+
+                // 釋放資源並關閉 SDL
+                close(); //
+                dialog = find_dialogue(dialogues, n_dialogue, event->dialog);
+                type = DIALOGUE;
                 break;
-            }
-        }
-
-        SDL_RenderPresent(gRenderer);  // 更新屏幕
-    }
-
-    // 显示结束场景并暂停
-    if (end_scene) {
-        bool pause = true;
-        printf("按 Ctrl+C 结束程序\n");
-        while (pause) {
-            while (SDL_PollEvent(&e) != 0) {
-                if (e.type == SDL_QUIT) {
-                    pause = false;
-                    quit = true;
+                
+            case DIALOGUE:
+                printf("\033[35m用 SDL 顯示對話:\033[0m %s\n", dialog->text);
+                if (dialog->character) {
+                    printf("\033[36m用 SDL 顯示角色:\033[0m %s\n", find_character(characters, n_character, dialog->character)->avatar);
                 }
-            }
+
+                if (dialog->n_opt > 0) {
+                    printf("\033[32m------------------------ 出現選項 ------------------------\033[0m\n");
+                    for (int i = 0; i < dialog->n_opt; i++) {
+                        printf("選項(%d): %s\n", i + 1, dialog->opts[i].text);
+                    }
+
+                    printf("請選擇一個, 你選擇: ");
+                    scanf("%d", &opt_number);
+                    opt_number = opt_number - 1 >= dialog->n_opt ? dialog->n_opt - 1 : opt_number - 1;
+                    opt = dialog->opts[opt_number];
+
+                    if (opt.event) {
+                        event = find_event(events, n_event, opt.event);
+                        type = EVENT;
+                    }
+                    else if (opt.next) {
+                        dialog = find_dialogue(dialogues, n_dialogue, opt.next);
+                        type = DIALOGUE;
+                    }
+                    else {
+                        printf("\033[31mdialogue's option format is error: '%s'\033[0m\n", dialog->alias);
+                    }
+                }
+
+                printf("\n");
+
+                if (dialog && strstr(dialog->alias, "ending")) {
+                    is_ending = true;
+                }
+
+                break;
+
+            default:
+                printf("\033[31mUnknow type\033[0m\n");
         }
     }
 
-    closeSDL();  // 关闭SDL和清理资源
-    return 0;
+    printf("\033[35m劇終囉 !!!\033[0m\n");
 }
 
-int initSDL() {
+int init() {
+    int success = 1;
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        return -1;
+        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+        success = 0;
+    } else {
+        gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (gWindow == NULL) {
+            printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+            success = 0;
+        } else {
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
+        }
     }
 
-    gWindow = SDL_CreateWindow("Interactive Fiction", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (!gWindow) {
-        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        return -1;
-    }
-
-    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
-    if (!gRenderer) {
-        SDL_DestroyWindow(gWindow);
-        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
-        return -1;
-    }
-
-    if (TTF_Init() == -1) {
-        SDL_DestroyRenderer(gRenderer);
-        SDL_DestroyWindow(gWindow);
-        printf("TTF could not initialize! TTF_Error: %s\n", TTF_GetError());
-        return -1;
-    }
-
-    gFont = TTF_OpenFont("font.ttf", 28);  // 确保font.ttf在项目目录下
-    if (!gFont) {
-        SDL_DestroyRenderer(gRenderer);
-        SDL_DestroyWindow(gWindow);
-        printf("Failed to load font! TTF_Error: %s\n", TTF_GetError());
-        return -1;
-    }
-
-    return 0;
+    return success;
 }
 
-void closeSDL() {
-    if (gFont) TTF_CloseFont(gFont);
-    if (gRenderer) SDL_DestroyRenderer(gRenderer);
-    if (gWindow) SDL_DestroyWindow(gWindow);
-    TTF_Quit();
+int loadMedia(char* imagePath1)
+{
+    // 加载成功标志
+    int success = 1;
+
+    // 加载第一张图像
+    gHelloWorld = SDL_LoadBMP(imagePath1);
+    if (gHelloWorld == NULL)
+    {
+        printf("无法加载图像 %s！SDL 错误：%s\n", imagePath1, SDL_GetError());
+        success = 0;
+    }
+
+    // 加载第二张图像
+    /*gOverlayImage = SDL_LoadBMP(imagePath2);
+    if (gOverlayImage == NULL)
+    {
+        printf("无法加载图像 %s！SDL 错误：%s\n", imagePath2, SDL_GetError());
+        success = 0;
+    }*/
+
+    return success; // 注意这里应该返回 success 而不是固定的 1
+}
+
+void close()
+{
+    // 釋放表面
+    SDL_FreeSurface( gHelloWorld );
+    gHelloWorld = NULL;
+
+    SDL_FreeSurface(gOverlayImage);
+    gOverlayImage = NULL;
+
+    // 銷毀窗口
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
+
+    // 退出 SDL 子系統
     SDL_Quit();
 }
 
-SDL_Texture* loadTexture(const char* path) {
-    SDL_Texture* newTexture = NULL;
-    SDL_Surface* loadedSurface = IMG_Load(path);
-    if (!loadedSurface) {
-        printf("Unable to load image %s! SDL Error: %s\n", path, IMG_GetError());
-    } else {
-        newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
-        if (!newTexture) {
-            printf("Unable to create texture from %s! SDL Error: %s\n", path, SDL_GetError());
+SDL_Surface* loadSurface(const char* path)
+{
+    SDL_Surface* optimizedSurface = NULL; // The final optimized image
+
+    // Load image at specified path
+    SDL_Surface* loadedSurface = SDL_LoadBMP(path);
+    if (loadedSurface == NULL)
+    {
+        printf("Unable to load image %s! SDL Error: %s\n", path, SDL_GetError());
+    }
+    else
+    {
+        // Convert surface to screen format
+        optimizedSurface = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, 0);
+        if (optimizedSurface == NULL)
+        {
+            printf("Unable to optimize image %s! SDL Error: %s\n", path, SDL_GetError());
         }
+
+        // Get rid of old loaded surface
         SDL_FreeSurface(loadedSurface);
     }
-    return newTexture;
-}
 
-void displayScene(SDL_Renderer* renderer, SDL_Texture* texture) {
-    SDL_RenderCopy(renderer, texture, NULL, NULL);
-}
-
-void displayText(SDL_Renderer* renderer, const char* text, int x, int y, SDL_Color color) {
-    SDL_Surface* surfaceMessage = TTF_RenderUTF8_Solid(gFont, text, color);
-    SDL_Texture* message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
-    SDL_Rect message_rect;
-    message_rect.x = x;
-    message_rect.y = y;
-    message_rect.w = surfaceMessage->w;
-    message_rect.h = surfaceMessage->h;
-    SDL_RenderCopy(renderer, message, NULL, &message_rect);
-    SDL_FreeSurface(surfaceMessage);
-    SDL_DestroyTexture(message);
-}
-
-void displayDialogueBox(SDL_Renderer* renderer, int x, int y, int w, int h) {
-    SDL_Rect rect = {x, y, w, h};
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 200);  // 设置颜色为半透明黑色
-    SDL_RenderFillRect(renderer, &rect);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);  // 重置颜色为白色
-}
-
-void displayCharacter(SDL_Renderer* renderer, SDL_Texture* texture) {
-    SDL_Rect destRect = {SCREEN_WIDTH / 2 - 150, SCREEN_HEIGHT - 450, 300, 400};  // 设置人物图片的位置和大小
-    SDL_RenderCopy(renderer, texture, NULL, &destRect);
+    return optimizedSurface;
 }
